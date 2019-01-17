6f0d92d22ef95c8fa496a31dec448b6fb22185c5
Sergey <sgmak@me.com>
2019-01-17 19:31:57 +0300

    -L9-P-EX-2/2. DateTime

---
+++ Homework_9/B-9-Extention-Methods-Deconstruction/B-9-Extention-Methods-Deconstruction/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Base_Lesson_9.Extentions;
+
+namespace Base_Lesson_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Practice.Practice.L9_P_EX_1_from_2();
+            Practice.Practice.L9_P_EX_2_from_2();
+            //Lesson.Lesson.DeconstructExample();
+            Console.ReadKey();
+        }
+    }
+}
