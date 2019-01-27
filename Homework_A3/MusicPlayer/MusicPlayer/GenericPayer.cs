65e76ecef3d08525d765ffc0a9d88e52763d5ae5
Sergey <sgmak@me.com>
2019-01-27 22:57:24 +0300

    GenericPlayer

---
+++ Homework_A3/MusicPlayer/MusicPlayer/GenericPayer.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MusicPlayer.Extantions;
+
+namespace MusicPlayer
+{
+    public class GenericPlayer<T>
+    {
+        public bool _isLocked;
+        public bool _isPlaying;
+        const int MIN_VOLUME = 0;
+        const int MAX_VOLUME = 100;
+        private ISkin skin;
+        internal ISkin Skin { get => skin; private set => skin = value; }
+        public List<T> Items; //B7-Player1/2. SongsListShuffle
+
+        public GenericPlayer()
+        {
+            Skin = new ClassicSkin();
+        }
+
+        public GenericPlayer(string color)
+        {
+            Skin = new ColorSkin(color);
+        }
+        private int _volume;
+        public int Volume
+        {
+            get
+            {
+                return _volume;
+            }
+
+            private set
+            {
+                if (value < MIN_VOLUME)
+                {
+                    _volume = MIN_VOLUME;
+                }
+                else if (value > MAX_VOLUME)
+                {
+                    _volume = MAX_VOLUME;
+                }
+                else
+                {
+                    _volume = value;
+                }
+            }
+        }
+
+        public void VolumeUp()
+        {
+            if (_isLocked == false)
+            {
+                Volume++;
+            }
+        }
+
+        public void VolumeDown()
+        {
+            if (_isLocked == false)
+            {
+                Volume--;
+            }
+        }
+
+        public void VolumeChange(int step)
+        {
+            if (_isLocked == false)
+            {
+                Volume += step;
+            }
+        }
+        public void Stop()
+        {
+            if (_isLocked)
+            {
+                return;
+            }
+            _isPlaying = false;
+            Skin.Render("Player has stopped");
+        }
+
+        public void Locked()
+        {
+            _isLocked = true;
+            Skin.Render("Player is locked");
+        }
+        public void Unlock()
+        {
+            _isLocked = false;
+            Skin.Render("Player is unlocked");
+        }
+
+        public virtual void Play()
+        {
+        }
+        public virtual void Play(List<T> playingItem)
+        {
+
+        }
+
+        public void Add(List<T> playingItem)
+        {
+            Items = playingItem;
+        }
+
+        //B7-Player1/2. SongsListShuffle
+        public void Shuffle() => this.Items = this.Items.ShuffleItem<T>();                     //L9 -HW -Player -1/3
+
+    }
+}
---
+++ Homework_A3/MusicPlayer/MusicPlayer/Song.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicPlayer
+{
+    public class Song : PlayingItem
+    {
+        
+        public Artist Artist;
+        public Album Album;
+        
+        public SongGenres genre;
+        //BL8 -Player3/3.SongGenres
+        public enum SongGenres
+        {
+            Classic = 0b00000001,
+            Rock = 0b00000010,
+            Pop = 0b00000100
+        }
+    }
+}
